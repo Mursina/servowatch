1aa97b7bc2f07ad3f18addc2b590bd0ea6c31f76
Mursina <fathimamursina2015@gmail.com>
2023-10-19 23:26:43 +0530

    Calculate the average price

--- test3/filtering.py
+++ test3/filtering.py
@@ -9,3 +9,19 @@ with open(input_file, "r", newline="") as csvfile:
     for row in reader:
         data.append(row)
 
+total_price_per_sqft = 0
+count = 0
+
+for row in data:
+    try:
+        price = float(row["price"])
+        sqft = float(row["sq__ft"])
+        if sqft != 0:  # Avoid division by zero
+            price_per_sqft = price / sqft
+            total_price_per_sqft += price_per_sqft
+            count += 1
+    except ValueError:
+        continue
+
+average_price_per_sqft = total_price_per_sqft / count
+
