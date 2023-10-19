bb10fe6ca908bd7d6bb7484261820d1d96a1b87a
Mursina <fathimamursina2015@gmail.com>
2023-10-19 12:26:04 +0530

    Get host name

--- monitor_services.py
+++ monitor_services.py
@@ -1,23 +1,25 @@
 import psutil
+import os
 
 services = ['httpd', 'postgres']
 status = []
 
+host_name = os.uname().nodename
+
 for service in services:
-    status_obj = {
-        "service_name":service,
-        "service_status":"DOWN",
-        "host_name":"host1"
-    }
+
+    service_status = "DOWN"
     
     for process in psutil.process_iter(attrs=['name']): 
         if service in process.info['name']:
-            status_obj ={
-                "service_name":service,
-                "service_status":"UP",
-                "host_name":"host1"
-            }
+            service_status = "UP"
             break
-
+    
+    status_obj = {
+        "service_name":service,
+        "service_status":service_status,
+        "host_name":host_name
+    }
     status.append(status_obj)
+
 print(status)
